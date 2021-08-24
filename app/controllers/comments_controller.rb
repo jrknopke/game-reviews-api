d394a8dd0d21bb00d8b751a6ccba88f4b2cc054a
Joseph Knopke <josephknopke@gmail.com>
2021-08-24 15:38:03 -0500

    comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,10 @@
 class CommentsController < ApplicationController
-    
+    def create
+        commment = Comment.new(comment_params)
+        if review.save
+            render json: Review.all.to_json(:include => :comments)
+        else
+            render json: {error: "Error"}
+        end
+    end
 end
