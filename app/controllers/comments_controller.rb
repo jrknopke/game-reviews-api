9150b0649c6f3707b4cd60a6b1173b41bc425242
Joseph Knopke <josephknopke@gmail.com>
2021-08-24 22:25:57 -0500

    comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
     def create
-        commment = Comment.new(comment_params)
-        if review.save
+        comment = Comment.new(comment_params)
+        if comment.save
             render json: Review.all.to_json(:include => :comments)
         else
             render json: {error: "Error"}
         end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :likes, :review_id)
     end
 end
